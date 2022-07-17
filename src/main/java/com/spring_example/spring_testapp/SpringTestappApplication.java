42f22c1108eacbd601592a99ed745311a1df605e
KiYari <rdztern@gmail.com>
2022-07-17 17:41:49 +0300

    spring project initialization

---
+++ src/main/java/com/spring_example/spring_testapp/SpringTestappApplication.java
@@ -0,0 +1,13 @@
+package com.spring_example.spring_testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_example/spring_testapp/SpringTestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_example.spring_testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
